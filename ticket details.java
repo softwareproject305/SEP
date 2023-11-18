c4323cfc462d48f6d55ee5dec30379ed3806aff1
softwareproject305 <softwaregithubproject@gmail.com>
2023-11-18 04:27:02 +0300

    First commit

--- ticket details.java
+++ ticket details.java
@@ -2,17 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+// Define a Ticket class to represent flight tickets
 class Ticket {
     private String passengerName;
     private String seatNumber;
     private String ticketNumber;
 
+    // Constructor to initialize ticket details
     public Ticket(String passengerName, String seatNumber, String ticketNumber) {
         this.passengerName = passengerName;
         this.seatNumber = seatNumber;
         this.ticketNumber = ticketNumber;
     }
 
+    // Getter methods to access ticket details
     public String getPassengerName() {
         return passengerName;
     }
@@ -26,17 +29,21 @@ class Ticket {
     }
 }
 
+// Main class for flight ticket details management
 public class FlightTicketDetails {
-    private List<Ticket> tickets;
+    private List<Ticket> tickets;  // List to store ticket objects
 
+    // Constructor to initialize the list of tickets
     public FlightTicketDetails() {
         tickets = new ArrayList<>();
     }
 
+    // Method to add a ticket to the list
     public void addTicket(Ticket ticket) {
         tickets.add(ticket);
     }
 
+    // Method to find a ticket by ticket number
     public Ticket findTicket(String ticketNumber) {
         for (Ticket ticket : tickets) {
             if (ticket.getTicketNumber().equals(ticketNumber)) {
@@ -46,6 +53,7 @@ public class FlightTicketDetails {
         return null;
     }
 
+    // Method to display all ticket details
     public void displayTicketDetails() {
         System.out.println("Ticket Details:");
         for (Ticket ticket : tickets) {
@@ -55,11 +63,13 @@ public class FlightTicketDetails {
         }
     }
 
+    // Main method to run the program
     public static void main(String[] args) {
-        FlightTicketDetails flight = new FlightTicketDetails();
-        Scanner scanner = new Scanner(System.in);
-        String choice;
+        FlightTicketDetails flight = new FlightTicketDetails();  // Create an instance of FlightTicketDetails
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object to read user input
+        String choice;  // Variable to store user's menu choice
 
+        // Display menu and process user input until the user chooses to exit
         do {
             System.out.println("1. Add Ticket");
             System.out.println("2. Find Ticket");
@@ -70,6 +80,7 @@ public class FlightTicketDetails {
 
             switch (choice) {
                 case "1":
+                    // Add a ticket
                     System.out.print("Enter passenger name: ");
                     String passengerName = scanner.nextLine();
                     System.out.print("Enter seat number: ");
@@ -80,7 +91,9 @@ public class FlightTicketDetails {
                     flight.addTicket(ticket);
                     System.out.println("Ticket added successfully!");
                     break;
+
                 case "2":
+                    // Find a ticket
                     System.out.print("Enter ticket number: ");
                     ticketNumber = scanner.nextLine();
                     Ticket foundTicket = flight.findTicket(ticketNumber);
@@ -91,19 +104,24 @@ public class FlightTicketDetails {
                         System.out.println("Ticket not found!");
                     }
                     break;
+
                 case "3":
+                    // Display all ticket details
                     flight.displayTicketDetails();
                     break;
+
                 case "4":
+                    // Exit the program
                     System.out.println("Exiting...");
                     break;
+
                 default:
                     System.out.println("Invalid choice!");
             }
 
             System.out.println();
-        } while (!choice.equals("4"));
+        } while (!choice.equals("4"));  // Repeat until the user chooses to exit
 
-        scanner.close();
-    }
+        scanner.close();  // Close the Scanner object
+    }
 }
